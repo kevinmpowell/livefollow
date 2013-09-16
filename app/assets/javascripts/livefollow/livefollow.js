6b87c2f4f4e7e6e7a52720b29bd0c745327d6d53
Kevin Powell <kevin@underthebedstudios.com>
2013-09-16 12:38:46 -0500

    Add ability to disable/enable livefollow for a browser/session via cookie params set via a hash

--- app/assets/javascripts/livefollow/livefollow.js
+++ app/assets/javascripts/livefollow/livefollow.js
@@ -6,37 +6,49 @@ if (typeof String.prototype.startsWith != 'function') {
 }
 
 $(document).ready(function(){
-  $("body").on("click", "a", function(e){
-    var url = $(this).attr("href");
-    if (!url.startsWith('#')) {
-      e.preventDefault();
-      $.ajax({
-        url: '/livefollow/set_url/' + encodeURIComponent(url),
-        success: function() {
-          window.location = url;
-        }
-      });      
-    }
-  });
+  if(window.location.hash && window.location.hash == '#lfdisable') {
+    $.cookie('livefollow_status', 'disabled');
+    alert("Livefollow disabled for this browser/session");
+  }
+  else if (window.location.hash && window.location.hash == '#lfenable'){
+    $.cookie('livefollow_status', 'enabled');
+    alert("Livefollow enabled for this browser/session");
+  }
+  console.log(typeof $.cookie('livefollow_status'));
+  console.log($.cookie('livefollow_status'));
 
-  if(window.location.hash && window.location.hash == '#!') {
-    $.ajax({
-      url: '/livefollow/set_url/' + encodeURIComponent(window.location.pathname),
-      success: function() {
-        poll_for_url_update();
+  if (typeof $.cookie('livefollow_status') == undefined || $.cookie('livefollow_status') == 'enabled') {
+    $("body").on("click", "a", function(e){
+      var url = $(this).attr("href");
+      if (!url.startsWith('#')) {
+        e.preventDefault();
+        $.ajax({
+          url: '/livefollow/set_url/' + encodeURIComponent(url),
+          success: function() {
+            window.location = url;
+          }
+        });      
       }
     });
-  }
-  else {
-    poll_for_url_update();
-  }
 
-
+    if(window.location.hash && window.location.hash == '#!') {
+      $.ajax({
+        url: '/livefollow/set_url/' + encodeURIComponent(window.location.pathname),
+        success: function() {
+          poll_for_url_update();
+        }
+      });
+    }
+    else {
+      poll_for_url_update();
+    }
+  }
 });
 
 function poll_for_url_update(){
   var current_url = window.location.pathname;
   window.setInterval(function(){
+    console.log("polling");
     $.ajax({
       url: '/livefollow/get_url',
       success: function(data) {
