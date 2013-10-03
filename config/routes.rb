c9f70abf19c4a2a2342663aebca97a9bb1eb9937
Kevin Powell <kevin@underthebedstudios.com>
2013-10-03 07:32:02 -0500

    Add ability for URLs with underscores to also work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Livefollow::Engine.routes.draw do
   match '/get_url' => 'livefollow#get_url'
-  match '/set_url/:pathname' => 'livefollow#set_url', constraints: {pathname: /[A-Za-z0-9%\-\.]+?/, format: /html/}
+  match '/set_url/:pathname' => 'livefollow#set_url', constraints: {pathname: /[A-Za-z0-9%\-\.\_]+?/, format: /html/}
 end
